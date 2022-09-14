587fe1fc9ce2291a3de9b0a7e57210a149c7aa14
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-14 18:36:19 -0400

    added padding to the stat-block

--- src/components/stat-block/stat-block.styles.tsx
+++ src/components/stat-block/stat-block.styles.tsx
@@ -14,6 +14,8 @@ align-items:center;
 z-index:999;
 top:calc(35% - 64.5px);
 box-shadow: 0px 50px 50px -25px rgba(0, 0, 0, 0.0983665);
+padding-left:1em;
+padding-right:1em;
 & > *:not(:last-child) {
     border-right: 1px solid #00000015;
 }
